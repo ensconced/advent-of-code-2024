93cad177509021824d061583aec3d76646951f70
Joe Barnett <joe.barnett@elliptic.co>
2024-12-02 10:34:14 +0000

    wip

--- projects/day2/lib.c
+++ projects/day2/lib.c
@@ -7,7 +7,7 @@ bool interval_is_safe(unsigned long prev, unsigned long curr, bool increasing) {
   int a = (int)prev;
   int b = (int)curr;
   int diff = increasing ? b - a : a - b;
-  return (diff <= 3 && diff > 0);
+  return (diff <= 3 && diff >= 1);
 }
 
 bool part1_line_is_safe(parsed_line line) {
@@ -38,64 +38,48 @@ int part1(char *input_path) {
   return safe_lines;
 }
 
-bool part2_make_interval_safe(size_t j, parsed_line line, bool *increasing,
+bool part2_make_interval_safe(size_t *j, parsed_line line, bool increasing,
                               int *dampeners) {
-  unsigned long prev = line.numbers[j - 1];
-  unsigned long curr = line.numbers[j];
+  unsigned long prev = line.numbers[*j - 1];
+  unsigned long curr = line.numbers[*j];
 
-  if (interval_is_safe(prev, curr, *increasing))
+  if (interval_is_safe(prev, curr, increasing))
     return true;
 
   if (*dampeners > 0) {
-    // if we're looking at the first or last interval, it's always fixable
-    if (j == 1 || j == line.length - 1) {
+    // if we're looking at the first or last interval, it's always fixable by
+    // dropping the first or last element
+    if (*j == 1 || *j == line.length - 1) {
       (*dampeners)--;
       return true;
     }
 
-    // what if we removed prev?
-    unsigned long prevprev = line.numbers[j - 2];
-    if (interval_is_safe(prevprev, curr, *increasing)) {
+    // can we fix it by skipping prev?
+    unsigned long prevprev = line.numbers[*j - 2];
+    if (interval_is_safe(prevprev, curr, increasing)) {
       (*dampeners)--;
       return true;
     }
 
-    // what if we removed curr?
-    unsigned long next = line.numbers[j + 1];
-    if (interval_is_safe(prev, next, *increasing)) {
+    // can we fix it by skipping curr?
+    unsigned long next = line.numbers[*j + 1];
+    if (interval_is_safe(prev, next, increasing)) {
       (*dampeners)--;
+      (*j)++;
       return true;
     }
-
-    // if (j == 1) {
-    //   *increasing = !*increasing;
-
-    //   // what if we removed prev?
-    //   unsigned long prevprev = line.numbers[j - 2];
-    //   if (interval_is_safe(prevprev, curr, *increasing)) {
-    //     (*dampeners)--;
-    //     return true;
-    //   }
-
-    //   // what if we removed curr?
-    //   unsigned long next = line.numbers[j + 1];
-    //   if (interval_is_safe(prev, next, *increasing)) {
-    //     (*dampeners)--;
-    //     return true;
-    //   }
-    // }
   }
 
   return false;
 }
 
-bool part2_line_is_safe(parsed_line line, int *dampeners) {
+bool part2_line_is_safe(parsed_line line, bool increasing) {
   if (line.length < 2)
     return true;
 
-  bool increasing = true;
+  int dampeners = 1;
   for (size_t j = 1; j < line.length; j++) {
-    if (!part2_make_interval_safe(j, line, &increasing, dampeners))
+    if (!part2_make_interval_safe(&j, line, increasing, &dampeners))
       return false;
   }
   return true;
@@ -106,9 +90,8 @@ int part2(char *input_path) {
 
   int safe_lines = 0;
   for (size_t i = 0; i < input.length; i++) {
-    int dampeners = 1;
     parsed_line line = input.lines[i];
-    if (part2_line_is_safe(line, &dampeners)) {
+    if (part2_line_is_safe(line, true) || part2_line_is_safe(line, false)) {
       safe_lines++;
     }
   }
