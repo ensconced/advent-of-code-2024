31c8ab9c805835e5baaf75d4ad1ad043745dced7
Joe Barnett <joe.barnett@elliptic.co>
2024-12-03 05:58:49 +0000

    remove dead files

--- projects/day3/parser.c
+++
@@ -1,6 +0,0 @@
-#include "../utils/file_utils/lib.h"
-#include "../utils/parser_utils/lib.h"
-#include "./lib.h"
-#include <stdio.h>
-
-void parse_input(char *input_path) { printf("%s\n", input_path); }
--- projects/day3/parser.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "./lib.h"
-
-void parse_input(char *input_path);
