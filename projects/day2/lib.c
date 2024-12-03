215ef4b68a869f0e87898773b27b050f8e948aa1
Joe Barnett <joe.barnett@elliptic.co>
2024-12-03 05:11:30 +0000

    finish day 2 - not optimised

--- projects/day2/lib.c
+++ projects/day2/lib.c
@@ -4,6 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+parsed_line copy_line(parsed_line line) {
+  unsigned long *numbers = malloc(sizeof(unsigned long *) * line.length);
+  for (size_t i = 0; i < line.length; i++) {
+    numbers[i] = line.numbers[i];
+  }
+  return (parsed_line){
+      .numbers = numbers,
+      .length = line.length,
+  };
+}
+
+void delete_at_index(parsed_line *line, size_t index) {
+  for (size_t i = index; i < line->length - 1; i++) {
+    line->numbers[i] = line->numbers[i + 1];
+  }
+  line->length--;
+}
+
 bool interval_is_safe(unsigned long prev, unsigned long curr, bool increasing) {
   int a = (int)prev;
   int b = (int)curr;
@@ -11,71 +29,40 @@ bool interval_is_safe(unsigned long prev, unsigned long curr, bool increasing) {
   return (interval <= 3 && interval >= 1);
 }
 
-bool interval_can_be_made_safe_using_dampener(size_t i, parsed_line line,
-                                              bool increasing) {
-  unsigned long prev = line.numbers[i - 1];
-  unsigned long curr = line.numbers[i];
-
-  // first, check if the following interval is also dodgy. if so, our only
-  // chance is to remove the node in the middle of the two dodgy intervals...
-
-  // if not, we can remove either the node before the interval or the node
-  // after the interval - whichever doesn't create another bad interval...
-
-  // if we're looking at the first or last interval, it's always fixable by
-  // dropping the first or last element.
-  // if (*j == 1 || *j == line.length - 1) {
-  //   (*dampeners)--;
-  //   return true;
-  // }
-
-  // can we fix it by skipping prev?
-  unsigned long prevprev = line.numbers[i - 2];
-  if (interval_is_safe(prevprev, curr, increasing)) {
-    return true;
-  }
-
-  // can we fix it by skipping curr?
-  unsigned long next = line.numbers[i + 1];
-  if (interval_is_safe(prev, next, increasing)) {
-    (*j)++;
-    return true;
-  }
-
-  return false;
-}
-
-bool line_is_safe(parsed_line line, bool increasing, bool can_use_dampener) {
+bool line_is_safe(parsed_line line, bool increasing) {
   if (line.length < 2)
     return true;
 
   for (size_t i = 1; i < line.length; i++) {
     unsigned long prev = line.numbers[i - 1];
     unsigned long curr = line.numbers[i];
-
-    if (interval_is_safe(prev, curr, increasing)) {
-      continue;
-    }
-
-    if (can_use_dampener &&
-        interval_can_be_made_safe_using_dampener(i, line, increasing)) {
-      can_use_dampener = false;
-      continue;
-    }
-
-    return false;
+    if (!interval_is_safe(prev, curr, increasing))
+      return false;
   }
   return true;
 }
 
-int count_safe_lines(char *input_path, bool can_use_dampener) {
+bool line_is_safe_with_dampener(parsed_line line, bool increasing) {
+  if (line_is_safe(line, increasing))
+    return true;
+
+  for (size_t i = 0; i < line.length; i++) {
+    parsed_line copy = copy_line(line);
+    delete_at_index(&copy, i);
+    if (line_is_safe(copy, increasing))
+      return true;
+  }
+
+  return false;
+}
+
+int part1(char *input_path) {
   parsed_input input = parse_input(input_path);
 
   int safe_lines = 0;
   for (size_t i = 0; i < input.length; i++) {
     parsed_line line = input.lines[i];
-    if (line_is_safe(line, true, can_use_dampener) ||
-        line_is_safe(line, false, can_use_dampener)) {
+    if (line_is_safe(line, true) || line_is_safe(line, false)) {
       safe_lines++;
     }
   }
@@ -83,5 +70,17 @@ int count_safe_lines(char *input_path, bool can_use_dampener) {
   return safe_lines;
 }
 
-int part1(char *input_path) { return count_safe_lines(input_path, false); }
-int part2(char *input_path) { return count_safe_lines(input_path, true); }
+int part2(char *input_path) {
+  parsed_input input = parse_input(input_path);
+
+  int safe_lines = 0;
+  for (size_t i = 0; i < input.length; i++) {
+    parsed_line line = input.lines[i];
+    if (line_is_safe_with_dampener(line, true) ||
+        line_is_safe_with_dampener(line, false)) {
+      safe_lines++;
+    }
+  }
+
+  return safe_lines;
+}
--- projects/day2/test.c
+++ projects/day2/test.c
@@ -4,10 +4,9 @@
 #include <stdio.h>
 
 int main(void) {
-  // printf("testing day 2\n");
-  // assert(part1("./inputs/example_input.txt") == 2);
-  // assert(part1("./inputs/input.txt") == 379);
-  // assert(part2("./inputs/example_input.txt") == 4);
-  // assert(part2("./inputs/test_input.txt") == 1);
-  printf("result: %d\n", part2("./inputs/input.txt"));
+  printf("testing day 2\n");
+  assert(part1("./inputs/example_input.txt") == 2);
+  assert(part1("./inputs/input.txt") == 379);
+  assert(part2("./inputs/example_input.txt") == 4);
+  assert(part2("./inputs/input.txt") == 430);
 }
