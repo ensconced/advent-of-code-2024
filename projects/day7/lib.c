0f158ddf1132f403ef717d4b8b541f3f203bd086
Joe Barnett <joe.barnett@elliptic.co>
2024-12-07 11:45:26 +0000

    day 7 part 2 :)

--- projects/day7/lib.c
+++ projects/day7/lib.c
@@ -7,68 +7,116 @@ typedef struct node {
   size_t depth;
 } node;
 
-unsigned long min(node n, line input_line) {
+unsigned long max(unsigned long a, unsigned long b, unsigned long c) {
+  if (a >= b && a >= c)
+    return a;
+  if (b >= a && b >= c)
+    return b;
+  return c;
+}
+
+unsigned long min(unsigned long a, unsigned long b, unsigned long c) {
+  if (a <= b && a <= c)
+    return a;
+  if (b <= a && b <= c)
+    return b;
+  return c;
+}
+
+unsigned long concat(unsigned long a, unsigned long b) {
+  char result[128];
+  snprintf(result, 100, "%lu%lu", a, b);
+  return strtoul(result, NULL, 10);
+}
+
+unsigned long lower_bound(node n, line input_line, bool allow_concat) {
   unsigned long result = n.acc;
   for (size_t i = n.depth; i < input_line.operands_len; i++) {
     unsigned long mult_result = result * input_line.operands[i];
     unsigned long sum_result = result + input_line.operands[i];
-    if (mult_result > sum_result) {
-      result = sum_result;
-    } else {
-      result = mult_result;
-    }
+    unsigned long concat_result =
+        allow_concat ? concat(result, input_line.operands[i]) : sum_result;
+    result = min(mult_result, sum_result, concat_result);
   }
   return result;
 }
 
-unsigned long max(node n, line input_line) {
+unsigned long upper_bound(node n, line input_line, bool allow_concat) {
   unsigned long result = n.acc;
   for (size_t i = n.depth; i < input_line.operands_len; i++) {
     unsigned long mult_result = result * input_line.operands[i];
     unsigned long sum_result = result + input_line.operands[i];
-    if (mult_result < sum_result) {
-      result = sum_result;
-    } else {
-      result = mult_result;
-    }
+    unsigned long concat_result =
+        allow_concat ? concat(result, input_line.operands[i]) : sum_result;
+    result = max(mult_result, sum_result, concat_result);
   }
   return result;
 }
 
-bool leads_to_solution(node n, line input_line) {
+bool leads_to_solution(node n, line input_line, bool allow_concat) {
   if (n.depth == input_line.operands_len)
     return n.acc == input_line.expected;
 
-  if (min(n, input_line) > input_line.expected)
+  if (lower_bound(n, input_line, allow_concat) > input_line.expected)
     return false;
 
-  if (max(n, input_line) < input_line.expected)
+  if (upper_bound(n, input_line, allow_concat) < input_line.expected)
     return false;
 
-  return leads_to_solution(
-             (node){
-                 .acc = n.acc + input_line.operands[n.depth],
-                 .depth = n.depth + 1,
-             },
-             input_line) ||
-         leads_to_solution(
-             (node){
-                 .acc = n.acc * input_line.operands[n.depth],
-                 .depth = n.depth + 1,
-             },
-             input_line);
+  bool sum_solution = leads_to_solution(
+      (node){
+          .acc = n.acc + input_line.operands[n.depth],
+          .depth = n.depth + 1,
+      },
+      input_line, allow_concat);
+
+  if (sum_solution)
+    return true;
+
+  bool mult_solution = leads_to_solution(
+      (node){
+          .acc = n.acc * input_line.operands[n.depth],
+          .depth = n.depth + 1,
+      },
+      input_line, allow_concat);
+
+  if (mult_solution)
+    return true;
+
+  if (allow_concat) {
+    bool concat_solution = leads_to_solution(
+        (node){
+            .acc = concat(n.acc, input_line.operands[n.depth]),
+            .depth = n.depth + 1,
+        },
+        input_line, allow_concat);
+    if (concat_solution)
+      return true;
+  }
+  return false;
 }
 
-bool is_valid(line input_line) {
+bool is_valid(line input_line, bool allow_concat) {
   node start_node = {.acc = input_line.operands[0], .depth = 1};
-  return leads_to_solution(start_node, input_line);
+  return leads_to_solution(start_node, input_line, allow_concat);
 }
 
 unsigned long part1(char *input_path) {
   parsed_input input = parse_input(input_path);
   unsigned long sum = 0;
   for (size_t i = 0; i < input.len; i++) {
-    if (is_valid(input.lines[i])) {
+    if (is_valid(input.lines[i], false)) {
+      sum += input.lines[i].expected;
+    }
+  }
+  return sum;
+}
+
+unsigned long part2(char *input_path) {
+  parsed_input input = parse_input(input_path);
+  unsigned long sum = 0;
+  for (size_t i = 0; i < input.len; i++) {
+    if (is_valid(input.lines[i], true)) {
       sum += input.lines[i].expected;
     }
   }
--- projects/day7/test.c
+++ projects/day7/test.c
@@ -7,4 +7,6 @@ int main(void) {
   printf("testing day 7\n");
   assert(part1("./inputs/example_input.txt") == 3749);
   assert(part1("./inputs/input.txt") == 12940396350192);
+  assert(part2("./inputs/example_input.txt") == 11387);
+  assert(part2("./inputs/input.txt") == 106016735664498);
 }
