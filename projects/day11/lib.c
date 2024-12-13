027480e0b5d89ccfd739c2eabe59bd083b1a1e2e
Joe Barnett <joe.barnett@elliptic.co>
2024-12-13 06:53:57 +0000

    day 11 part 2

--- projects/day11/lib.c
+++ projects/day11/lib.c
@@ -3,45 +3,70 @@
 #include "./parser.h"
 #include <stdio.h>
 
+#define CACHE_HEIGHT 75
+#define CACHE_WIDTH 4028
+
 unsigned long long digit_count(unsigned long long num) {
   return (unsigned long long)log10((double)num) + 1;
 }
 
-size_t size_after_blinks(unsigned long long *stones, size_t stones_len,
-                         size_t blink_count) {
-  for (size_t i = 0; i < blink_count; i++) {
-    unsigned long long *new_stones =
-        malloc(sizeof(unsigned long long) * stones_len * 2);
-    size_t new_stones_len = 0;
-    for (size_t j = 0; j < stones_len; j++) {
-      unsigned long long stone = stones[j];
-      if (stone == 0) {
-        new_stones[new_stones_len++] = 1;
-      } else {
-        unsigned long long digits = digit_count(stone);
-        if (digits % 2 == 0) {
-          unsigned long long power =
-              (unsigned long long)pow(10, (double)(digits / 2));
-          unsigned long long lhs = stone / power;
-          unsigned long long rhs = stone % power;
-          new_stones[new_stones_len++] = lhs;
-          new_stones[new_stones_len++] = rhs;
-        } else {
-          new_stones[new_stones_len++] = stone * 2024;
-        }
-      }
-    }
-    stones = new_stones;
-    stones_len = new_stones_len;
+size_t size_after_blinks(unsigned long long stone, size_t blink_count,
+                         unsigned long long *cache) {
+  if (blink_count == 0)
+    return 1;
+
+  if (stone < CACHE_WIDTH && blink_count < CACHE_HEIGHT) {
+    unsigned long long cached_result = cache[blink_count * CACHE_WIDTH + stone];
+    if (cached_result)
+      return cached_result;
   }
-  return stones_len;
+
+  size_t result;
+
+  if (stone == 0) {
+    result = size_after_blinks(1, blink_count - 1, cache);
+  } else {
+    unsigned long long digits = digit_count(stone);
+    if (digits % 2 == 0) {
+      unsigned long long power =
+          (unsigned long long)pow(10, (double)(digits / 2));
+      unsigned long long lhs = stone / power;
+      unsigned long long rhs = stone % power;
+      result = size_after_blinks(lhs, blink_count - 1, cache) +
+               size_after_blinks(rhs, blink_count - 1, cache);
+    } else {
+      result = size_after_blinks(stone * 2024, blink_count - 1, cache);
+    }
+  }
+
+  if (stone < CACHE_WIDTH && blink_count < CACHE_HEIGHT) {
+    cache[blink_count * CACHE_WIDTH + stone] = result;
+  }
+  return result;
+}
+
+unsigned long long *create_cache() {
+  unsigned long long *cache =
+      calloc(CACHE_WIDTH * CACHE_HEIGHT, sizeof(unsigned long long));
+  return cache;
 }
 
 size_t part1(char *input_path) {
   parsed_input input = parse_input(input_path);
-  return size_after_blinks(input.numbers, input.len, 25);
+  unsigned long long *cache = create_cache();
+  size_t result = 0;
+  for (size_t i = 0; i < input.len; i++) {
+    result += size_after_blinks(input.numbers[i], 25, cache);
+  }
+  return result;
 }
+
 size_t part2(char *input_path) {
   parsed_input input = parse_input(input_path);
-  return size_after_blinks(input.numbers, input.len, 75);
+  unsigned long long *cache = create_cache();
+  size_t result = 0;
+  for (size_t i = 0; i < input.len; i++) {
+    result += size_after_blinks(input.numbers[i], 75, cache);
+  }
+  return result;
 }
--- projects/day11/test.c
+++ projects/day11/test.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 int main(void) {
+  printf("testing day 11\n");
   assert(part1("./inputs/example_input.txt") == 55312);
   assert(part1("./inputs/input.txt") == 199986);
-  // printf("part 2: %lu\n", part2("./inputs/input.txt"));
+  assert(part2("./inputs/input.txt") == 236804088748754);
 }
